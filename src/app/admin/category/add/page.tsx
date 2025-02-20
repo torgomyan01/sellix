c2f7a88b7b0b4531c1a7ce29748f3c42a9f5dfcf
Andranik Torgomyan <and.torgomyan01@gmail.com>
2025-02-20 14:54:20 +0400

    Fix bugs

--- src/app/admin/category/add/page.tsx
+++ src/app/admin/category/add/page.tsx
@@ -1,8 +1,18 @@
 import MainTemplateAdmin from "@/app/admin/main-template-admin";
 import React from "react";
+import { Button, TextField } from "@mui/material";
 
 function AddCategory() {
-  return <MainTemplateAdmin>Add Category</MainTemplateAdmin>;
+  return (
+    <MainTemplateAdmin>
+      <form action="#">
+        <div className="mb-3">
+          <TextField label="Անվանում" className="w-[400]" variant="outlined" />
+        </div>
+        <Button variant="contained">Ավելացնել</Button>
+      </form>
+    </MainTemplateAdmin>
+  );
 }
 
 export default AddCategory;
--- src/app/admin/category/page.tsx
+++ src/app/admin/category/page.tsx
@@ -1,8 +1,33 @@
+"use client";
+
 import MainTemplateAdmin from "@/app/admin/main-template-admin";
 import React from "react";
 
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { allCategories } from "@/utils/consts";
+import { Box } from "@mui/material";
+
 function AddCategory() {
-  return <MainTemplateAdmin>Category</MainTemplateAdmin>;
+  return (
+    <MainTemplateAdmin>
+      <Autocomplete
+        options={allCategories}
+        sx={{ width: 300 }}
+        getOptionLabel={(option) => option.name}
+        renderOption={(props, option) => {
+          const { key, ...optionProps } = props;
+          return (
+            <Box key={key} component="li" {...optionProps}>
+              <i className={`fa-solid ${option.icon} mr-2`}></i>
+              {option.name}
+            </Box>
+          );
+        }}
+        renderInput={(params) => <TextField {...params} label="Կատեգորիաներ" />}
+      />
+    </MainTemplateAdmin>
+  );
 }
 
 export default AddCategory;
